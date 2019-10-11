619096a2e0586b8aa44aff38551ccb10b82dc37a
d1741 <traciecodes@gmail.com>
2019-10-10 22:17:00 -0500

    Initial commit: tested server, set up session config

--- api/server.js
+++ api/server.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
-
-const authRouter = require('../auth/auth-router.js');
-const usersRouter = require('../users/users-router.js');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const session = require("express-session");
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
 
 const server = express();
 
@@ -11,11 +11,26 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-server.use('/api/auth', authRouter);
-server.use('/api/users', usersRouter);
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
 
-server.get('/', (req, res) => {
-  res.json({ api: 'up' });
+const sessionConfig = {
+  name: "trackme", //for session id (can be anything)
+  secret: "monsoon demons, messing around", //encryption key for sessions...should technically never be hard-coded
+  cookie: {
+    maxAge: 1000 * 60 * 60, //how long should the cookie last?
+    secure: false, //http = false, https = true,
+    httpOnly: true // browser can't access cookie by viewing as a document file
+  },
+  resave: false, //so expired sessions can't be resaved
+  saveUninitialized: false //lets people know we put cookies on their machine (must be false)
+};
+
+//use the middleware we made by calling session and passingin our own session config
+server.use(session(sessionConfig));
+
+server.get("/", (req, res) => {
+  res.json({ api: "up" });
 });
 
 module.exports = server;
